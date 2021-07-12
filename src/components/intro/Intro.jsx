6c26537325fe13b23219f18f02d22709af387ea0
aritonkaramani <ariton.karamani@gmail.com>
2021-07-12 12:21:02 +0200

    new commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,14 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            
+            <div className="left">
+                <div className="imgContainer">
+                <img src="public/assets/small-img.png" alt=""></img>
+                    container 1111
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
