6bed30acbd8e4eaf971601545e32afaca69f7f08
aritonkaramani <ariton.karamani@gmail.com>
2021-08-06 14:42:40 +0200

    form works

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,22 +11,24 @@ export default function Contact() {
     return (
         <div className="contact" id="contact">
             <div className="card">
-            <div className="left">
+                <div className="left">
                     <MeetingIcon className="icon"/>
 
-            </div>
-            <div className="right">
-                <h2>Contact</h2>
-                <form action="mail.php" method="post" >
-                    <input type="text" name="name" placeholder="Your name"/>
-                    <input type="email" name="emailaddress" placeholder="Email" />
-                    <textarea type="text" name="message" placeholder="Message"></textarea>
-                    <button type="submit">Send</button>
-                   
-                </form>
-            </div>
+             </div>
+                <div className="right">
+                    <h2>Get in Touch</h2>
+                    <div id="contact-form">
+                        <form action="https://formspree.io/f/mrgryjgj" method="POST">
+                        <input type="hidden" name="_subject" value="Contact request from Portfolio Website" />
+                        <input type="name" name="_name" placeholder="Your name" required/>
+                        <input type="email" name="_replyto" placeholder="Your email" required/>
+                        <textarea name="message" placeholder="Type your message" required></textarea>
+                        <button type="submit">Send</button>
+                        </form>
+                    </div>
             </div>
         </div>
+    </div>
     )
 }
 /*  {message && <span>Thanks, I will reply as soon as possible</span>} */
